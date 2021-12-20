ab1307e12f6928d19486d4ce980c76b25d25b5a8
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-12-20 12:41:01 +0545

    demo: Add radio buttons to change file source

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,17 @@
 	import { Document, Page } from '$lib';
 	let zoomLevel: number = 1;
 	let pageNumber: number = 1;
+	let docUrl = '/tackling-ts-preview-book.pdf';
 </script>
 
 <input type="range" step="0.25" max="4" min="1" bind:value={zoomLevel} />
 <input type="number" bind:value={pageNumber} />
-<Document file="/tackling-ts-preview-book.pdf">
+<input type="radio" value="/tackling-ts-preview-book.pdf" bind:group={docUrl} /> Doc 1
+<input type="radio" value="/impatient-js-preview-book.pdf" bind:group={docUrl} /> Doc 2
+
+<Document file={docUrl}>
 	<div>
-		<Page {zoomLevel} {pageNumber}/>
+		<Page {zoomLevel} {pageNumber} />
 	</div>
 </Document>
 
