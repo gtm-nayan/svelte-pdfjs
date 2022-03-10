3c356fc09fbf2f307bb4a507bf3028256916fb05
gtm-nayan <gtmnayan@gmail.com>
2022-03-10 15:16:16 +0545

    Organize imports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,9 @@
 export { default as Document, key } from './PDFViewer/Document.svelte';
 export { default as Page } from './PDFViewer/Page.svelte';
 export * from './utils/target_dimension.js';
+export { PDFJS };
 
 import * as PDFJS from 'pdfjs-dist';
 if (PDFJS.GlobalWorkerOptions) {
 	PDFJS.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${PDFJS.version}/build/pdf.worker.min.js`;
 }
-export { PDFJS };
