719cd170820325467ea66db7194ce85fdcd9af6e
gtmnayan <gtmnayan@gmail.com>
2022-12-10 17:10:29 +0545

    fix: use URL constructor for worker src

--- src/lib/utils/vite.ts
+++ src/lib/utils/vite.ts
@@ -1,10 +1,13 @@
 import { PDFWorker } from 'pdfjs-dist';
-import PDFWorkerSrc from 'pdfjs-dist/build/pdf.worker.min.js?url';
 import { onDestroy, setContext } from 'svelte';
 
 export function set_pdfjs_context() {
 	if (!import.meta.env.SSR) {
-		const worker = new PDFWorker({ port: new Worker(PDFWorkerSrc) as unknown as null });
+		const worker = new PDFWorker({
+			port: new Worker(
+				new URL('pdfjs-dist/build/pdf.worker.min.js', import.meta.url)
+			) as unknown as null,
+		});
 		setContext('svelte_pdfjs_worker', worker);
 		onDestroy(() => worker.destroy());
 	}
