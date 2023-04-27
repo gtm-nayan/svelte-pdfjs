9b7d8a8c323bbda6240653b72b71c24866907ce7
gtmnayan <gtmnayan@gmail.com>
2023-04-27 22:18:39 +0545

    ...

--- src/lib/PDFViewer/Page.svelte
+++ src/lib/PDFViewer/Page.svelte
@@ -12,50 +12,54 @@ Render a page from a PDF document. Must be a child of a `Document` component.
 	import '@gtmnayan/pdfjs-dist-esm/web/pdf_viewer.css';
 
 	import type { PDFDocumentProxy } from '@gtmnayan/pdfjs-dist-esm';
-	import { getContext, onMount } from 'svelte';
+	import { getContext, onDestroy, onMount } from 'svelte';
 	import type { Writable } from 'svelte/store';
 
-	import type { PDFSinglePageViewer } from '@gtmnayan/pdfjs-dist-esm/web/pdf_single_page_viewer.js';
-	import { EventBus } from '@gtmnayan/pdfjs-dist-esm/web/event_utils.js';
+	import type {
+		EventBus,
+		PDFSinglePageViewer,
+	} from '@gtmnayan/pdfjs-dist-esm/web/pdf_viewer.component.js';
 	import { browser } from '$app/environment';
 </script>
 
 <script lang="ts">
 	const current_doc: Writable<PDFDocumentProxy> = getContext('svelte_pdfjs_doc');
 
-	let PDFSinglePageViewer: typeof import('@gtmnayan/pdfjs-dist-esm/web/pdf_single_page_viewer.js');
-
 	let container_el: HTMLDivElement;
 	let viewer: PDFSinglePageViewer;
 
-	let eventBus: EventBus;
+	export let eventBus: EventBus;
 
 	export let num: number;
 	export let scale = 1;
 	export let container_styles: string;
 	export let render_text_layer = false;
 
-	onMount(() => {
-		(async () => {
-			eventBus = new EventBus();
-			({ PDFSinglePageViewer } = await import(
-				'@gtmnayan/pdfjs-dist-esm/web/pdf_single_page_viewer.js'
-			));
-			viewer = new PDFSinglePageViewer({
-				container: container_el,
-				eventBus,
-			});
-		})();
+	async function create_viewer() {
+		const { EventBus, PDFSinglePageViewer } = await import(
+			'@gtmnayan/pdfjs-dist-esm/web/pdf_viewer.component.js'
+		);
+		eventBus = new EventBus();
 
-		return () => {
-			viewer?.cleanup();
-			eventBus?.reset();
-		};
+		// @ts-expect-error
+		viewer = new PDFSinglePageViewer({
+			container: container_el,
+			eventBus,
+		});
+	}
+	onMount(create_viewer);
+
+	onDestroy(() => {
+		viewer?.cleanup();
+		// @ts-expect-error
+		viewer = null;
+		// @ts-expect-error
+		eventBus = null;
 	});
 
 	$: browser && viewer && viewer.setDocument($current_doc);
 	$: browser && viewer && Reflect.set(viewer, 'currentPageNumber', num);
-	$: browser && viewer && Reflect.set(viewer, 'currentScale', scale);
+	$: browser && viewer && Reflect.set(viewer, 'currentScaleValue', scale);
 </script>
 
 <div class="container" bind:this={container_el} style={container_styles}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,71 +15,73 @@
 	let sizing = 1;
 </script>
 
-<section class="settings">
-	Page <input type="number" bind:value={num} step="1" min="1" max={max_pages} /> of {max_pages}
+<main>
+	<section class="settings">
+		Page <input type="number" bind:value={num} step="1" min="1" max={max_pages} /> of {max_pages}
 
-	<select bind:value={filename}>
-		<option>tackling-ts-preview-book.pdf</option>
-		<option>impatient-js-preview-book.pdf</option>
-		<option value="a.pdf">non existent file</option>
-	</select>
-
-	<input type="checkbox" bind:checked={renderTextLayer} /> Render text layer
-
-	<fieldset>
-		<legend>Dimensions</legend>
-		<select bind:value={sizing}>
-			<option value={1}>Scale: {scale}x</option>
-			<option value={2}>
-				Fixed height: {target_height}px
-			</option>
+		<select bind:value={filename}>
+			<option>tackling-ts-preview-book.pdf</option>
+			<option>impatient-js-preview-book.pdf</option>
+			<option value="a.pdf">non existent file</option>
 		</select>
 
-		{#if sizing === 2}
-			<input type="range" bind:value={target_height} min="200" max="700" step="50" />
-		{:else}
-			<input type="range" bind:value={scale} min="0.5" max="4" step="0.25" />
-		{/if}
+		<input type="checkbox" bind:checked={renderTextLayer} /> Render text layer
 
-		<label>
-			Rotation
-			<select bind:value={rotation}>
-				<option>0</option>
-				<option>90</option>
-				<option>180</option>
-				<option>270</option>
+		<fieldset>
+			<legend>Dimensions</legend>
+			<select bind:value={sizing}>
+				<option value={1}>Scale: {scale}x</option>
+				<option value={2}>
+					Fixed height: {target_height}px
+				</option>
 			</select>
-		</label>
-	</fieldset>
-</section>
 
-{#if browser}
-	<!-- {base}/ isn't neceassary if your app lives at the root of your host. 
+			{#if sizing === 2}
+				<input type="range" bind:value={target_height} min="200" max="700" step="50" />
+			{:else}
+				<input type="range" bind:value={scale} min="0.5" max="4" step="0.25" />
+			{/if}
+
+			<label>
+				Rotation
+				<select bind:value={rotation}>
+					<option>0</option>
+					<option>90</option>
+					<option>180</option>
+					<option>270</option>
+				</select>
+			</label>
+		</fieldset>
+	</section>
+
+	<div>
+		{#if browser}
+			<!-- {base}/ isn't neceassary if your app lives at the root of your host. 
 	---- However we're serving this demo through github pages so the pdfs will
 	---- be at /svelte-pdfjs/filename.pdf
 	-->
-	<Document
-		file="{base}/{filename}"
-		loadOptions={{ docBaseUrl: base }}
-		on:loadsuccess={(e) => {
-			max_pages = e.detail.numPages;
-			num = Math.min(num, max_pages);
-		}}
-		on:loaderror={(e) => alert(e.detail + '')}
-	>
-		<div>
-			<Page
-				{scale}
-				{num}
-				{renderTextLayer}
-				{rotation}
-				getViewport={sizing === 1 ? undefined : preferThisHeight(target_height)}
-			/>
-		</div>
-	</Document>
-{/if}
+			<Document
+				file="{base}/{filename}"
+				loadOptions={{ docBaseUrl: base }}
+				on:loadsuccess={(e) => {
+					max_pages = e.detail.numPages;
+					num = Math.min(num, max_pages);
+				}}
+				on:loaderror={(e) => alert(e.detail + '')}
+			>
+				<Page {num} {scale} />
+			</Document>
+		{/if}
+	</div>
+</main>
 
 <style>
+	main {
+		display: grid;
+		height: 100vh;
+		grid-template-rows: auto 1fr;
+	}
+
 	div {
 		display: grid;
 		place-items: center;
@@ -87,6 +89,6 @@
 	}
 
 	div > :global(.container) {
-		top: 0;
+		inset: 0;
 	}
 </style>
